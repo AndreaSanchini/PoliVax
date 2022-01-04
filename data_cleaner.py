92b2414aaa17c5285e7e573739e5c79d2c13bdc8
andrea_sanchini <andrea.sanchini@mail.polimi.it>
2022-01-04 15:55:42 +0100

    dropped some useless columns in owid_data

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,21 +1,26 @@
 import pandas as pd
 
-data = pd.read_csv('data/owid_covid_data.csv')
+df = pd.read_csv('data/owid_covid_data.csv')
 
-data = data[(data.iso_code != 'OWID_AFR') & (data.iso_code != 'OWID_ASI') & (data.iso_code != 'OWID_EUN')
-            & (data.iso_code != 'OWID_EUR') & (data.iso_code != 'OWID_HIC') & (data.iso_code != 'OWID_INT')
-            & (data.iso_code != 'OWID_KOS') & (data.iso_code != 'OWID_LIC') & (data.iso_code != 'OWID_LMC')
-            & (data.iso_code != 'OWID_CYN') & (data.iso_code != 'OWID_NAM') & (data.iso_code != 'OWID_OCE')
-            & (data.iso_code != 'OWID_SAM') & (data.iso_code != 'OWID_UMC') & (data.iso_code != 'OWID_WRL')]
+df = df[(df.iso_code != 'OWID_AFR') & (df.iso_code != 'OWID_ASI') & (df.iso_code != 'OWID_EUN')
+        & (df.iso_code != 'OWID_EUR') & (df.iso_code != 'OWID_HIC') & (df.iso_code != 'OWID_INT')
+        & (df.iso_code != 'OWID_KOS') & (df.iso_code != 'OWID_LIC') & (df.iso_code != 'OWID_LMC')
+        & (df.iso_code != 'OWID_CYN') & (df.iso_code != 'OWID_NAM') & (df.iso_code != 'OWID_OCE')
+        & (df.iso_code != 'OWID_SAM') & (df.iso_code != 'OWID_UMC') & (df.iso_code != 'OWID_WRL')]
+df = df.drop(['reproduction_rate', 'icu_patients', 'icu_patients_per_million', 'hosp_patients',
+              'hosp_patients_per_million', 'weekly_icu_admissions', 'weekly_icu_admissions_per_million',
+              'weekly_hosp_admissions', 'weekly_hosp_admissions_per_million', 'stringency_index', 'aged_65_older',
+              'aged_70_older', 'cardiovasc_death_rate', 'diabetes_prevalence', 'female_smokers', 'male_smokers',
+              'handwashing_facilities', 'extreme_poverty'], axis=1)
 
-data.to_csv('data/owid_covid_data_cleaned.csv')
+df.to_csv('data/owid_covid_data_cleaned.csv')
 
-data = pd.read_csv('data/eu_vaccinations.csv')
+df = pd.read_csv('data/eu_vaccinations.csv')
 
-data = data[data['Region'].isin(['AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI', 'FR', 'UNK', 'HR',
-                                 'HU', 'IE', 'IS', 'IT', 'LI', 'LT', 'LU', 'LV', 'MT', 'NL', 'NO', 'PL', 'PT', 'RO',
-                                 'SE', 'SI', 'SK'])]
-data['ReportingCountry'] = data['ReportingCountry'].replace({'EL': 'GR'})
-data = data.drop(['FirstDoseRefused'], axis=1)
+df = df[df['Region'].isin(['AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI', 'FR', 'UNK', 'HR',
+                           'HU', 'IE', 'IS', 'IT', 'LI', 'LT', 'LU', 'LV', 'MT', 'NL', 'NO', 'PL', 'PT', 'RO',
+                           'SE', 'SI', 'SK'])]
+df['ReportingCountry'] = df['ReportingCountry'].replace({'EL': 'GR'})
+df = df.drop(['FirstDoseRefused'], axis=1)
 
-data.to_csv('data/eu_vaccinations_cleaned.csv')
+df.to_csv('data/eu_vaccinations_cleaned.csv')
